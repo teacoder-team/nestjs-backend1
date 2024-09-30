137fc994ce02c42c7749b9ef464105371b210f16
TeaCoder52 <vnechaev888@gmail.com>
2024-09-30 05:31:50 +0300

    update format date util

--- src/libs/common/utils/format-date.util.ts
+++ src/libs/common/utils/format-date.util.ts
@@ -5,5 +5,8 @@ export function formatDate(dateString: Date | string) {
 	const month = String(date.getMonth() + 1).padStart(2, '0')
 	const year = date.getFullYear()
 
-	return `${day}.${month}.${year}`
+	const hours = String(date.getHours()).padStart(2, '0')
+	const minutes = String(date.getMinutes()).padStart(2, '0')
+
+	return `${day}.${month}.${year} в ${hours}:${minutes}`
 }
